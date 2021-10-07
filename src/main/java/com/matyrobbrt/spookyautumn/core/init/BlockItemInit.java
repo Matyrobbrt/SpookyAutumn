55a97f14c3eb60cb0491bc4609ccde6a94bb182b
ABlackPikatchu <77450514+ABlackPikatchu@users.noreply.github.com>
2021-10-07 08:30:19 -0400

    forgot block items

--- src/main/java/com/matyrobbrt/spookyautumn/core/init/BlockItemInit.java
+++ src/main/java/com/matyrobbrt/spookyautumn/core/init/BlockItemInit.java
@@ -12,8 +12,11 @@ public class BlockItemInit {
 			BlockInit.SPOOKY_BLOCK.get(), BlockInit.MAPLE_LOG.get(), BlockInit.MAPLE_PLANKS.get(),
 			BlockInit.MAPLE_WOOD.get(), BlockInit.STRIPPED_MAPLE_LOG.get(), BlockInit.STRIPPED_MAPLE_WOOD.get(),
 			BlockInit.BEECH_LOG.get(), BlockInit.BEECH_PLANKS.get(), BlockInit.BEECH_WOOD.get(),
-			BlockInit.STRIPPED_BEECH_LOG.get(), BlockInit.STRIPPED_BEECH_WOOD.get(),
-			BlockInit.HICKORY_LOG.get(), BlockInit.HICKORY_PLANKS.get(), BlockInit.HICKORY_WOOD.get(),
-			BlockInit.STRIPPED_HICKORY_LOG.get(), BlockInit.STRIPPED_HICKORY_WOOD.get());
+			BlockInit.STRIPPED_BEECH_LOG.get(), BlockInit.STRIPPED_BEECH_WOOD.get(), BlockInit.HICKORY_LOG.get(),
+			BlockInit.HICKORY_PLANKS.get(), BlockInit.HICKORY_WOOD.get(), BlockInit.STRIPPED_HICKORY_LOG.get(),
+			BlockInit.STRIPPED_HICKORY_WOOD.get(), BlockInit.AUTUMN_MAPLE_LEAVES.get(), BlockInit.MAPLE_SAPLING.get(),
+			BlockInit.AUTUMN_BEECH_LEAVES.get(), BlockInit.BEECH_SAPLING.get(), BlockInit.AUTUMN_HICKORY_LEAVES.get(),
+			BlockInit.HICKORY_SAPLING.get(), BlockInit.AUTUMN_OAK_LEAVES.get(), BlockInit.AUTUMN_OAK_SAPLING.get(),
+			BlockInit.AUTUMN_BIRCH_LEAVES.get(), BlockInit.AUTUMN_BIRCH_SAPLING.get());
 
 }
